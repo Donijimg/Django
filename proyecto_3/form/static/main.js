d9597dd8d8c3c257222aa762edf0bad864a0e478
JUNIORALAN <padillajunior080.co>
2024-02-26 20:59:12 -0500

    errores

--- proyecto_3/form/static/main.js
+++ proyecto_3/form/static/main.js
@@ -3,32 +3,48 @@ const firs_name = document.getElementById("Nombre");
 const last_name = document.getElementById("Apellidos");
 const correo = document.getElementById("email");
 const phone = document.getElementById("telefono");
+const mujer = document.getElementById("mujer");
+const hombre = document.getElementById("hombre");
+const otro = document.getElementById("otro");
+const ciudad = document.getElementById("ciudad")
+const html = document.getElementById("html")
+const css = document.getElementById("css")
+const php = document.getElementById("php")
+const javascript = document.getElementById("javascript")
+const java = document.getElementById("Java") 
+const sql = document.getElementById("sql")
+const uml = document.getElementById("uml")
 
-function validarficha(valor) {
+
+
+function validarficha(ficha) {
   const paraficha = /^[A-Za-z]{3}_[0-9]{5}$/;
-  if (!paraficha.test(valor)) {
+  if (!paraficha.test(ficha.value)) {
     alert(
-      "Ingrese un valor válido para el número de ficha solo letras, máximo 10 caracteres."
+      "ingrese un su ficha solo 3 letras un _ numeros de 0 al 9 limite 5"
     );
   } else {
     alert("¡Correcto!");
   }
 }
 
-function validarname(valor) {
-  const paraname = /^[A-Za-z]{50}$/;
-  if (!paraname.test(valor)) {
-    alert(
-      "ingrese un su nombre maximo 50 letras"
-    );
-  } else {
-    alert("¡Correcto!");
+function validarname(firs_name) {
+  const paraname = /^[a-zA-Z]*$/;
+  if (!paraname.value == "") {
+    alert("Enter a name");
+    firs_name.value.focus();
+    return false;
+  }
+  if (!paraname.test(e.value)) {
+    alert("Invalid characters");
+    firs_name.name.focus();
+    return false;
   }
 }
 
-function validarlast(valor) {
+function validarlast(last_name) {
   const paralastname = /^[A-Za-z]{50}$/;
-  if (!paralastname.test(valor)) {
+  if (!paralastname.test(last_name)) {
     alert(
       "ingrese un su apellido maximo 50 letras"
     );
@@ -37,15 +53,17 @@ function validarlast(valor) {
   }
 }
 
-function restrictNumber(e) {
-  var newValue = this.value.replace(new RegExp(/[^\d]/, 'ig'), "");
-  this.value = newValue;
+
+function restrictNumber() {
+  const number = this.value.replace(new RegExp(/[^\d]/, 'ig'), "");
+  this.value = number;
 }
 
 
-function validarEmail(valor) {
+function validarEmail(correo) {
   const paraemail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-  if (!paraemail.test(valor)) {
+
+  if (!paraemail.test(correo)) {
     alert(
       "ingrese un su apellido maximo 50 letras"
     );
@@ -56,6 +74,7 @@ function validarEmail(valor) {
 }
 
 phone.addEventListener('input', restrictNumber);
+
 ficha.addEventListener("blur", () => {
   validarficha(ficha.value);
 });
@@ -65,47 +84,13 @@ firs_name.addEventListener("blur", () => {
 });
 
 last_name.addEventListener("blur", () => {
-  validarname(last_name.value);
+  validarlast(last_name.value);
+});
+
+correo.addEventListener("blur", () => {
+  validarEmail(correo.value);
 });
 
 
 
-
-`
-const bordeVerde = function (valor) {
-  if (valor.classList.contains("borde-rojo")) {
-    valor.classList.remove("borde-rojo");
-  }
-  valor.classList.add("borde-verde");
-}
-
-const bordeRojo = function (valor) {
-  if (valor.classList.contains("borde-verde")) {
-    valor.classList.remove("borde-verde");
-  }
-  valor.classList.add("borde-rojo");
-}
-`
-
 // nombre.addEventListener(keypress,letras)
-
-
-// function onlyAlphabets(e, t) {
-//   try {
-//     if (window.event) {
-//       var charCode = window.event.keyCode;
-//     }
-//     else if (e) {
-//       var charCode = e.which;
-//     }
-//     else { return true; }
-//     if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123))
-//       return true;
-//     else
-//       return false;
-//   }
-//   catch (err) {
-//     alert(err.Description);
-//   }
-// }
-// name
